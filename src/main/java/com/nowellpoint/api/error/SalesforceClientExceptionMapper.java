f509147eaac1cefc6edc85607adeae6db7f8774e
John Herson <john_herson@rapid7.com>
2019-10-14 14:02:55 -0400

    added travis.yml

--- src/main/java/com/nowellpoint/api/error/SalesforceClientExceptionMapper.java
+++ src/main/java/com/nowellpoint/api/error/SalesforceClientExceptionMapper.java
@@ -2,7 +2,6 @@ package com.nowellpoint.api.error;
 
 import com.nowellpoint.client.sforce.SalesforceClientException;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
@@ -11,8 +10,8 @@ public class SalesforceClientExceptionMapper implements ExceptionMapper<Salesfor
 
 	@Override
 	public Response toResponse(SalesforceClientException exception) {
-		return Response.status(Status.BAD_REQUEST)
-				.entity(exception.getMessage())
+		return Response.status(exception.getStatusCode())
+				.entity(exception.getErrors())
 				.build();
 	}
 }
